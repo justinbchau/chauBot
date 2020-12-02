06d7f8197f5b8211fbb72bd098e90a3e28f98b5c
Justin <justinchau31@gmail.com>
2020-12-02 13:58:44 -0800

    Added logic and commands

---
+++ commands/socials.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'socials',
+	description: 'Social Media Accounts',
+	execute(message, args) {
+		message.channel.send('YouTube: https://youtube.com/c/chaucodes');
+	},
+};
---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const { prefix } = require('./config.json')
+const fs = require('fs');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	bot.commands.set(command.name, command);
+}
+
+
+
+
+
+bot.login(TOKEN);
+
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+
+
+bot.on('message', msg => {
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+    if (!bot.commands.has(command)) return;
+
+    try {
+		bot.commands.get(command).execute(msg, args);
+	} catch (error) {
+		console.error(error);
+		msg.reply('there was an error trying to execute that command!');
+	}
+
+
+})
